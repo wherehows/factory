f77dd5b9a232885f3f7580b934fd7cf87acf17d6
wherehows <sensitive.hu@gmail.com>
2023-03-18 16:16:07 +0900

    chore: useBackspace 훅 및 backspaceAtom에 jsdoc 추가

--- apps/nextjs/atoms/backspaceAtom.ts
+++ apps/nextjs/atoms/backspaceAtom.ts
@@ -1,9 +1,28 @@
 import { atom } from "jotai";
-
+/**
+ * Determine whether beforePopState event is triggered or not
+ */
 export let isBeforePopStateEventTriggered = false;
 
-export const updateIsBeforePopStateEventTriggered = (nextState) => isBeforePopStateEventTriggered = nextState;
+/**
+ * Update isBeforePopStateEventTriggered
+ * 
+ * This function is only called in beforePopState event handler
+ */
+export const updateIsBeforePopStateEventTriggered = (nextState: boolean) => isBeforePopStateEventTriggered = nextState;
 
+/**
+ * State to decide whether the current page has been reached through a backword navigation or not
+ * 
+ * The setter function for this state is only invoked in the 'routeChangeStart' event handler, 
+ * and the state is only set to 'true' if the {@link isBeforePopStateEventTriggered `isBeforePopStateEventTriggered`} flag is 'true'.
+ */
 export const isCurrentPageVisitedByBackspaceAtom = atom(false);
 
+/**
+ * Determine whether routeChange event has finished or not
+ * 
+ * The setter function for this state is invoked in the 'routeChangeStart' event handler for setting 'true'
+ * and also invoked in 'routeChangeComplete' event handler for setting 'false'
+ */
 export const isRoutingAtom = atom(false);
--- apps/nextjs/hooks/useBackspace.ts
+++ apps/nextjs/hooks/useBackspace.ts
@@ -1,50 +1,55 @@
 import {
-    isBeforePopStateEventTriggered,
-    isCurrentPageVisitedByBackspaceAtom,
-    updateIsBeforePopStateEventTriggered,
-    isRoutingAtom
+  isBeforePopStateEventTriggered,
+  isCurrentPageVisitedByBackspaceAtom,
+  updateIsBeforePopStateEventTriggered,
+  isRoutingAtom
 } from '../atoms/backspaceAtom';
 import { useSetAtom } from 'jotai';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 const useBackspace = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const setIsCurrentPageVisitedByBackspace = useSetAtom(
-        isCurrentPageVisitedByBackspaceAtom
-    );
+  const setIsCurrentPageVisitedByBackspace = useSetAtom(
+    isCurrentPageVisitedByBackspaceAtom
+  );
 
-    const setIsRouting = useSetAtom(isRoutingAtom);
+  const setIsRouting = useSetAtom(isRoutingAtom);
 
-    useEffect(() => {
-        updateIsBeforePopStateEventTriggered(false);
+  useEffect(() => {
+    // isBeforePopStateEventTriggered always must be initialized as 'false'
+    updateIsBeforePopStateEventTriggered(false);
 
-        const handleChangeRouteStart = () => {
-            setIsRouting(true);
+    const handleChangeRouteStart = () => {
+      // when route starts, isRouting state is set to 'true'
+      setIsRouting(true);
 
-            isBeforePopStateEventTriggered
-                ? setIsCurrentPageVisitedByBackspace(true)
-                : setIsCurrentPageVisitedByBackspace(false);
-        };
+      // depending on isBeforePopStateEventTriggerd variable, isCurrentPageVisitedByBackspace is set to 'true' or 'false'
+      isBeforePopStateEventTriggered
+        ? setIsCurrentPageVisitedByBackspace(true)
+        : setIsCurrentPageVisitedByBackspace(false);
+    };
 
-        const handleChangeRouteComplete = () => {
-            setIsRouting(false);
-        };
+    const handleChangeRouteComplete = () => {
+      // when route has finished, isRouting state is set to 'false'
+      setIsRouting(false);
+    };
 
-        router.beforePopState(() => {
-            updateIsBeforePopStateEventTriggered(true);
-            return true;
-        });
+    router.beforePopState(() => {
+      // update isBeforePopStateEventTriggerd variable as true only when beforePopState event is triggered
+      updateIsBeforePopStateEventTriggered(true);
+      return true;
+    });
 
-        router.events.on('routeChangeStart', handleChangeRouteStart);
-        router.events.on('routeChangeComplete', handleChangeRouteComplete);
+    router.events.on('routeChangeStart', handleChangeRouteStart);
+    router.events.on('routeChangeComplete', handleChangeRouteComplete);
 
-        return () => {
-            router.events.off('routeChangeStart', handleChangeRouteStart);
-            router.events.off('routeChangeComplete', handleChangeRouteComplete);
-        };
-    }, [router, setIsCurrentPageVisitedByBackspace, setIsRouting]);
+    return () => {
+      router.events.off('routeChangeStart', handleChangeRouteStart);
+      router.events.off('routeChangeComplete', handleChangeRouteComplete);
+    };
+  }, [router, setIsCurrentPageVisitedByBackspace, setIsRouting]);
 };
 
 export default useBackspace;
